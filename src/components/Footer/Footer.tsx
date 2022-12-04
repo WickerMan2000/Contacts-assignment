bbae9af6de874441667d830df1699649cda37230
Savvas <savaravant@gmail.com>
2022-12-04 20:53:35 +0200

    Add last ui extras

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export const Footer = () => {
     return (
         <div className={styles.footer}>
-            <p>Challenge by Speedcast</p>
+            <p>Challenge by <a href="https://www.speedcast.com/" target="_blank">Speedcast</a></p>
         </div>
     );
 };
